b0ca1ebdd8f588bcc5da786e4f2e75b41dd0f85d
Danny Cao <dannycao1997@yahoo.com>
2024-03-17 22:38:26 -0400

    added vector test

--- gettestie/src/test/java/rocks/zipcode/VectorTest.java
+++ gettestie/src/test/java/rocks/zipcode/VectorTest.java
@@ -1,4 +1,30 @@
 package rocks.zipcode;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Vector;
 
 public class VectorTest {
+    
+    @Test
+    public void testAdd(){
+        Vector<String> vector = new Vector<String>();
+
+        vector.add("string");
+
+        Assert.assertTrue(vector.contains("string"));
+    }
+
+
+
+    @Test
+    public void testRemove(){
+        Vector<Integer> vector = new Vector<Integer>();
+
+        vector.add(0, 1997);
+
+        vector.remove(0);
+
+        Assert.assertTrue(vector.isEmpty());
+    }
 }
+
