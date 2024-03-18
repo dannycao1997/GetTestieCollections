f10b1c307bf38eaffd16bf38e91b87aaea9fc3f9
Danny Cao <dannycao1997@yahoo.com>
2024-03-17 21:04:41 -0400

    added tests

--- gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
@@ -1,4 +1,33 @@
 package rocks.zipcode;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+
 
 public class ArrayListTest {
+    @Test
+    public void testAddAndGet(){
+        ArrayList<String> myList = new ArrayList<>();
+
+        // Test adding new elements
+        myList.add(0,"Zip");
+        myList.add(1, "Code");
+
+        // Verifying new elements are added
+        Assert.assertEquals("Zip", myList.get(0));
+        Assert.assertEquals("Code", myList.get(1));
+    }
+    @Test
+    public void testRemove(){
+        ArrayList<String> myList = new ArrayList<>();
+
+        // Test adding elements to index 0
+        myList.add(0,"HELLO WORLD");
+
+        // Test remove element from index 0
+        myList.remove("HELLO WORLD");
+
+        // Verifying string is removed from index 0
+        Assert.assertEquals(0, myList.size());
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/HashSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -1,4 +1,47 @@
 package rocks.zipcode;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class HashSetTest {
+
+    @Test
+    public void testAddAndRemove(){
+        Set<String> hashset = new HashSet<>();
+
+        // Adding Strings to hash set
+        hashset.add("Danny");
+        hashset.add("Cao");
+
+        // Removing string from has set
+        hashset.remove("Cao");
+
+        //Verifying string "Danny" was added to hash set
+        Assert.assertTrue(hashset.contains("Danny"));
+
+        //Verifying string "Cao" was removed from hash set
+        Assert.assertFalse(hashset.contains("Cao"));
+    }
+
+    @Test
+    public void testAddAndRemove2(){
+        Set<Integer> hashset = new HashSet<>();
+
+        // Adding integer to hash set
+        hashset.add(97);
+        hashset.add(9);
+
+        // Removing integer from hash set
+        hashset.remove(97);
+
+        // Verifying integer 9 was added to hash set
+        Assert.assertTrue(hashset.contains(9));
+
+        // Verifying integer 97 was removed from hash set
+        Assert.assertFalse(hashset.contains(97));
+    }
 }
+
+
